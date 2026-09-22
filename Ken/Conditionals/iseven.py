c34c19746b5831051db0d82fe6a4b28d90fcd011
Kennethdotse <kquame47@gmail.com>
2026-09-22 11:18:35 +0000

    Added my files

--- Ken/Conditionals/iseven.py
+++ Ken/Conditionals/iseven.py
@@ -1,3 +1,4 @@
+import math
 try:
     num = int(input("Enter a number: "))
 except:
@@ -11,3 +12,15 @@ elif num % 2 == 0:
     print(num, "is an even number.")
 else:
     print(num, "is an odd number.")
+    
+print(dir(math))
+    
+num = math.pow(3, 1/8)
+print(num)
+
+num = math.log10(1000)
+print(num)
+
+import random
+number = random.randint(2, 60)
+print(number)
